710a019e7abf33909a8ec885b86e629226849de3
MrsRibeiroGabriela <ribeirogabriela.dev@gmail.com>
2023-01-04 01:03:21 -0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'App.css';
+
+import Home from "./components/Home";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Milestones from "./components/Milestones";
+import Blog from "./components/Blog";
+import Video from "./components/Video";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div initial="hidden" animate="show">
+      <Home />
+      <Services />
+      <Portfolio />
+      <Milestones />
+      <Blog />
+      <Video />
+      <Pricing />
+      <Testimonials />
+      <Skills />
+      <Contact />
+      <Footer />
+      <ScrollToTop />
+    </motion.div>
   );
 }
 
